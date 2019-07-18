db9db497fbde4d849406f11bf426817f1ebc6a5f
ameliaknipp <ameliaknipp@aol.com>
2019-07-18 22:36:29 +0000

    Linked List

---
+++ linkedlist3.rb
@@ -0,0 +1,43 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def infinite_loop?(list)
+  tortoise = list.next_node
+  hare = list.next_node
+
+  until hare.nil?
+    hare = hare.next_node
+    return false if hare.nil?
+
+    hare = hare.next_node
+    tortoise = tortoise.next_node
+    return true if hare == tortoise
+  end
+
+  return false
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+
+puts infinite_loop?(node3)
+node1.next_node = node3 
+puts infinite_loop?(node3)
